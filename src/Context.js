9ded95de27a86524938035277823adeca7015cc0
ayushi-309 <tayushi751@gmail.com>
2026-05-27 14:51:07 +0530

    Add AppProvider to index.js and implement context in Context.js

--- src/Context.js
+++ src/Context.js
@@ -1,1 +1,21 @@
 // context <API> </API>
+// useContext hooks
+
+
+// context(warehouse)
+// Provider
+// consumer / (useContext())
+
+import React from 'react'
+
+const AppContext = React.createContext()
+
+//we need to create a provider function
+
+const AppProvider = ({children}) => {
+    return <AppContext.Provider value="HEllo world">
+        {children}
+    </AppContext.Provider>
+}
+
+export {AppContext, AppProvider}
